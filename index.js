1333c58433dad02eda7716e9c374337540223845
ayaali333 <1aya.dn.ali@gmail.com>
2022-08-14 16:01:37 +0300

    project11

---
+++ index.js
@@ -0,0 +1,24 @@
+let input  = document.getElementById('query');
+let input2 = document.getElementById('con');
+let button= document.getElementById('btn');
+
+
+
+// let user ='';
+//   input.addEventListener('input',(e)=>{
+//     // e.preventDefault();
+//     // let che = e.target.value;
+//     // input2.append(che);
+//      console.log(e.target.value)
+
+// })
+// console.log(user)
+
+
+// button.addEventListener('click',(e)=>{
+
+// })
+let x = input.value
+ console.log(x)
+ 
+
